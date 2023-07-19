edf42fdf8e6a9678fb6aed673752097cbfdbd7ed
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-19 12:26:11 +0100

    added basi linear jitter dataclass for primary training of restoregan

---
+++ src/dataset-classes/datasetLinear.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils.data import Dataset
+from ImageGenerator import ImageGenerator
+from JitterFilter import JitterFilter
+
+class JitteredDataset(Dataset):
+    def __init__(self, N, maxJitter, psfSigma=3, length=100):
+        self.N = N
+        self.length = length
+        self.psfSigma = psfSigma
+        self.maxJitter = maxJitter
+        self.Generator = ImageGenerator(self.N)
+        self.Filter = JitterFilter()
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        groundTruthNumpy = self.Generator.genericNoise(sigma=self.psfSigma)
+        jitteredTruthNumpy = self.Filter.rowJitter(groundTruthNumpy, self.N,
+                                                   self.maxJitter)
+
+        groundTruthTorch = torch.tensor(groundTruthNumpy, dtype=torch.float32) 
+        jitteredTruthTorch = torch.tensor(jitteredTruthNumpy, dtype=torch.float32) 
+
+        return groundTruthTorch, jitteredTruthTorch
+
+if __name__ == "__main__":
+    dataset = JitteredDataset(5, 2)
+    jittered, truth = dataset[0] 
+    print(jittered)
+    
