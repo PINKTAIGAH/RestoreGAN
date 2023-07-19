ad998276ec03b4dd5801d2d9df15e1ce6c90ba9d
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-19 12:38:44 +0100

    Added WGAN-gp model for discriminator

---
+++ src/discriminator.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules import padding
+
+"""
+Model for Discriminator
+"""
+
+class Discriminator(nn.Module):
+
+    def __init__(self, channelImages, featuresD):
+        super(Discriminator, self).__init__()
+        self.critic = nn.Sequential(
+            ### INPUT SIZE: N * channelImages * 64 * 64
+            nn.Conv2d(channelImages, featuresD, kernel_size=4,
+                      stride=2, padding= 1),
+            ### SIZE: 32*32
+            nn.LeakyReLU(0.2),
+            self._block(featuresD, featuresD*2, 4, 2, 1),
+            ### SIZE: 16*16
+            self._block(featuresD*2, featuresD*4, 4, 2, 1),
+            ### SIZE: 8*8
+            self._block(featuresD*4, featuresD*8, 4, 2, 1),
+            ### SIZE: 4*4
+            nn.Conv2d(featuresD*8, 1, kernel_size=4, stride=2, padding=0),
+            ### SIZE: 1*1
+        )
+    
+    ### Convolutional block that contains BatchNorm2d after a convolution
+    def _block(self, inChannels, outChannels, kernalSize, stride, padding):
+        ### This will be internal blocks of layers of the model
+        ### In and out channels are the channel input and output size
+        return nn.Sequential(
+            nn.Conv2d(inChannels, outChannels, kernalSize, 
+                      stride, padding, bias=False),
+            nn.BatchNorm2d(outChannels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.critic(x)
+
+"""
+Initialise weights for discriminator
+"""
+
+def initialiseWeights(model):
+    ### Setting init weights when doing one of the following operations
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
