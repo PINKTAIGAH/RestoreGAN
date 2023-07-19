9b7fa43846fc965d1d85ee4d8f1fb4572114f234
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-19 12:51:52 +0100

    Added model saving and loading models as well as saving example of images while training

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RestoreGAN
 An implementation of RestoreGAN to be used in image dejittering at the P06 experiment at PETRA III
+
+#### Note that code has been written such that it expects a single input images
+from the dataset. This would mean that the left image is the Jittered truth image
+while the right image is the unjittered ground truth
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import torch
+import config
+from torchvision.utils import save_image
 
 def gradientPenalty(discriminator, realImage, fakeImage, device=torch.device("cpu") ):
 
@@ -23,3 +25,45 @@ def gradientPenalty(discriminator, realImage, fakeImage, device=torch.device("cp
     
     return gradientPenalty
 
+"""
+Model saving/loading
+"""
+
+def saveCheckpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+
+def loadCheckpoint(checkpointFile, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpointFile, map_location=config.DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for paramGroup in optimizer.paramGroups:
+        paramGroup["lr"] = lr
+
+
+"""
+Saving examples of generated and ground truth images for easy viewing
+"""
+
+
+def saveSomeExamples(generator, validationLoader, epoch, folder):
+    x, y = next(iter(validationLoader))
+    x, y = x.to(config.DEVICE), y.to(config.DEVICE)
+    generator.eval()
+    with torch.no_grad():
+        yFake = generator(x)
+        yFake = yFake * 0.5 + 0.5  # remove normalization#
+        save_image(yFake, folder + f"/y_gen_{epoch}.png")
+        save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
+        if epoch == 1:
+            save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}.png")
+    generator.train()
