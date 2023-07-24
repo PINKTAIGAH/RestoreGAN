f7ea693880775b83fc3b0bb56b9552aff1c1f09d
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-24 22:14:10 +0100

    Fixed resnet blocks

--- src/neural-nets/generator.py
+++ src/neural-nets/generator.py
@@ -3,21 +3,21 @@ import torch.nn as nn
 
 class LargeBlock(nn.Module):
     
-    def __init__(self, inChannel=1):
+    def __init__(self, inChannel=1, outChannel=64, middleChannel=64):
         super().__init__()
         
         self.firstBlock = nn.Sequential(
-            nn.Conv2d(in_channels=inChannel, out_channels=64, kernel_size= 11, 
-                      stride=1, padding=0,), 
-            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=inChannel, out_channels=middleChannel,
+                      kernel_size= 11, stride=1, padding=0,), 
+            nn.BatchNorm2d(middleChannel),
             nn.ReLU(),
         )
 
         self.secondBlock = nn.Sequential(
             nn.MaxPool2d(kernel_size=7, stride=1, padding=0,),
-            nn.Conv2d(in_channels= 64, out_channels=64, kernel_size=7, stride=1, 
-                      padding=0),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels= middleChannel, out_channels=outChannel, 
+                      kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(outChannel),
             nn.ReLU(),
         )
 
@@ -28,13 +28,13 @@ class LargeBlock(nn.Module):
 
 class ThirdBlock(nn.Module):
 
-    def __init__(self, inChannel=64):
+    def __init__(self, inChannel=64, outChannel=128):
         super().__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels=inChannel, out_channels=128, kernel_size=3,
+            nn.Conv2d(in_channels=inChannel, out_channels=outChannel, kernel_size=3,
                       stride=2, padding=0),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(outChannel),
             nn.ReLU(),
         )
 
@@ -43,7 +43,7 @@ class ThirdBlock(nn.Module):
 
 class ResBlock(nn.Module):
      
-    def __init__(self, inChannel=128):
+    def __init__(self, inChannel=128, outChannel=128):
         super().__init__()
 
         self.resBlock = nn.Sequential(
@@ -51,24 +51,27 @@ class ResBlock(nn.Module):
                       padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1,
+            nn.Conv2d(in_channels=128, out_channels=outChannel, kernel_size=3, stride=1,
                       padding=1),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(outChannel),
         )
 
     def forward(self, x):
-        return self.resBlock(x)
+        identity = x
+        x = self.resBlock(x)
+        x+= identity
+        return x
 
 class SixthBlock(nn.Module):
 
-    def __init__(self, inChannel=256, A=1):
+    def __init__(self, inChannel=128, outChannel=2, A=1):
         super().__init__()
 
         self.sixthBlock = nn.Sequential(
-            nn.Conv2d(in_channels=inChannel, out_channels=2, kernel_size=5, stride=1,
-                      padding=0),
-            nn.BatchNorm2d(2),
-            nn.Tanh(),          #CHANGE TO EXPANDED TANH
+            nn.Conv2d(in_channels=inChannel, out_channels=outChannel, kernel_size=5,
+                      stride=1,padding=0),
+            nn.BatchNorm2d(outChannel),
+            nn.Tanh(),          # CHANGE TO EXPANDED TANH
         )
 
         self.A = A
@@ -87,9 +90,9 @@ class Generator(nn.Module):
         ### out channels ==> 128
         self.down3 = ResBlock(inChannel=128)
         ### out channel ==> 128 + 128 from  skip connection
-        self.down4 = ResBlock(inChannel=128*2)
+        self.down4 = ResBlock(inChannel=128)
         ### out channel ==> 128 + 128 from skip connection
-        self.down5 = SixthBlock(inChannel=128*2)
+        self.down5 = SixthBlock(inChannel=128)
         ### out channels = 2
     
 
@@ -104,16 +107,17 @@ class Generator(nn.Module):
         d3 = self.down3(d2)
         print(f"d3 shape ==> {d3.shape}")
 
-        d4 = self.down4(torch.cat([d3, d2], 1))
+        d4 = self.down4(d3)
         print(f"d4 shape ==> {d4.shape}")
 
-        d5 = self.down5(torch.cat([d4, d3], 1))
+        d5 = self.down5(d4)
         print(f"d5 shape ==> {d5.shape}")
 
         return d5 
 
 def test():
-    x = torch.randn((1, 1, 256, 256))
+    N = 256 
+    x = torch.randn((1, 1, N, N))
     model = Generator(inChannel=1)
     predicition = model(x)
     print(predicition.shape)
