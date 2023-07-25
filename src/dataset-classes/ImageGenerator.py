b74addee25cae8ee8fad676e8a4c50b6cc53b92e
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-25 13:32:59 +0100

    changed jitter generator codne

--- src/dataset-classes/ImageGenerator.py
+++ src/dataset-classes/ImageGenerator.py
@@ -3,7 +3,6 @@ import torch
 from skimage.filters import gaussian
 from scipy.signal import convolve2d
 
-
 class ImageGenerator(object):
     def __init__(self, N=64):
         self.N = N
@@ -13,7 +12,7 @@ class ImageGenerator(object):
         self.kernal[self.kernalSize//2, self.kernalSize//2] = 1
         self.kernal = gaussian(self.kernal, sigma=self.sigma)
 
-    def genericNoise(self, nConvolutions=3, kernalSize=4, sigma=3):
+    def genericNoise(self, nConvolutions=3, kernalSize=5, sigma=3):
         self.sigma = sigma
         self.kernalSize = kernalSize
         self.createKernalPSE()
@@ -21,10 +20,8 @@ class ImageGenerator(object):
         array = np.random.random((self.N, self.N))
 
         for _ in range(nConvolutions):
-            array = convolve2d(array, self.kernal, boundary="wrap")
+            array = convolve2d(array, self.kernal, boundary="wrap", mode="same")
         return array
 
 if __name__ == "__main__":
     Generator = ImageGenerator()
-    print(Generator.genericNoise())
-
