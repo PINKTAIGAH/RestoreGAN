dacf5391620cb734f90e05a2fa5eb77303a461ad
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-25 13:31:07 +0100

    rewrote utility and config folders

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,23 +1,58 @@
 import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from torch.utils.tensorboard.writer import SummaryWriter
+from torchvision.transforms import transforms
 
-"""
-Hyperparameter definiton
-"""
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")   # Change to GPU only in future
-TRAIN_DIR = None
-VALIDATION_DIR = None
-LEARNING_RATE = 1e-4
-BATCH_SIZE = 64         # This is subject to change
-N_WORKERS = 2
-IMAGE_SIZE = 265        # Variable
-IMAGE_CHANNELS = 1
-FEATURES_DISCRIMINATOR = 16
-FEATURES_GENERATOR = None       # Will have to investigate
-CRITIC_ITERATIONS = None        # Will have to investigate
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+TRAIN_DIR = "data/train"
+VAL_DIR = "data/val"
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 16
+NUM_WORKERS = 2
+IMAGE_SIZE = 256
+IMAGE_JITTER = 3
+CHANNELS_IMG = 1 
+L1_LAMBDA = 100
 LAMBDA_GP = 10
-N_EPOCH = 500                   # Will have to investigate
+NUM_EPOCHS = 500
+LOAD_MODEL = False 
 SAVE_MODEL = True
-LOAD_MODEL = False
-CHECKPOINT_DISC = "disc.pth.tar"
-CHECKPOINR_GEN = "gen.pth.tar"
+CHECKPOINT_DISC = "../models/disc.pth.tar"
+CHECKPOINT_GEN = "../models/gen.pth.tar"
+WRITER_REAL = SummaryWriter("../runs/real")
+WRITER_FAKE = SummaryWriter("../runs/fake")
+SOBEL_KERNAL = torch.tensor(
+    [
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ], dtype=torch.float32
+)
+
+both_transform = A.Compose(
+    [A.Resize(width=256, height=256),], additional_targets={"image0": "image"},
+)
+
+transform_only_input = A.Compose(
+    [
+        A.HorizontalFlip(p=0.5),
+        A.ColorJitter(p=0.2),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
+
+transform_only_mask = A.Compose(
+    [
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        ToTensorV2(),
+    ]
+)
+
+transforms = transforms.Compose([
+    transforms.Normalize(
+        [0.5 for _ in range(CHANNELS_IMG)],   # generalise for multi channel
+        [0.5 for _ in range(CHANNELS_IMG)],
+    ),
+])   
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,35 +1,23 @@
 import torch
 import config
 from torchvision.utils import save_image
+import torch.nn.functional as F
 
-def gradientPenalty(discriminator, realImage, fakeImage, device=torch.device("cpu") ):
+def save_some_examples(gen, val_loader, epoch, folder):
+    x, y = next(iter(val_loader))
+    x, y = x.to(config.DEVICE), y.to(config.DEVICE)
+    gen.eval()
+    with torch.no_grad():
+        y_fake = gen(x)
+        y_fake = y_fake * 0.5 + 0.5  # remove normalization#
+        save_image(y_fake, folder + f"/y_gen_{epoch}.png")
+        save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
+        if epoch == 1:
+            save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}.png")
+    gen.train()
 
-    BATCH_SIZE, C, H, W = realImage.shape  # Channel, Hight, Width
 
-    ### Create interpolated images (mix of real and fake with some random weight)
-    epsilon = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
-    interpolatedImages = realImage * epsilon + fakeImage * (1 - epsilon)
-
-    ### Calculate critic score
-    mixedScores = discriminator(interpolatedImages)
-
-    gradient = torch.autograd.grad(inputs=interpolatedImages,
-                                   outputs=mixedScores,
-                                   grad_outputs=torch.ones_like(mixedScores),
-                                   create_graph=True,
-                                   retain_graph=True)[0]
-
-    gradient = gradient.view(gradient.shape[0], -1)
-    gradientNorm = gradient.norm(2, dim=1)
-    gradientPenalty = torch.mean((gradientNorm-1)**2)
-    
-    return gradientPenalty
-
-"""
-Model saving/loading
-"""
-
-def saveCheckpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
     print("=> Saving checkpoint")
     checkpoint = {
         "state_dict": model.state_dict(),
@@ -38,32 +26,47 @@ def saveCheckpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
     torch.save(checkpoint, filename)
 
 
-def loadCheckpoint(checkpointFile, model, optimizer, lr):
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
     print("=> Loading checkpoint")
-    checkpoint = torch.load(checkpointFile, map_location=config.DEVICE)
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
     model.load_state_dict(checkpoint["state_dict"])
     optimizer.load_state_dict(checkpoint["optimizer"])
 
     # If we don't do this then it will just have learning rate of old checkpoint
     # and it will lead to many hours of debugging \:
-    for paramGroup in optimizer.paramGroups:
-        paramGroup["lr"] = lr
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+def tensorConcatinate(tensorLeft, tensorRight):
+    tensorRight = tensorRight.view(-1, tensorRight.shape[-1])
+    tensorLeft = tensorLeft.view(-1, tensorLeft.shape[-1])
+    return torch.cat((tensorLeft, tensorRight), dim=1) 
+
+def verticalEdges(inputTensor):
+    print(inputTensor.shape)
+    print(config.SOBEL_KERNAL.shape)
+    return F.conv2d(inputTensor.reshape(1, 1, *inputTensor.shape),
+                    config.SOBEL_KERNAL.reshape(1, 1, *config.SOBEL_KERNAL.shape),
+                    padding="same")
+
+def findMin(tensor):
+    N = tensor.shape[-1]
+    minVals, _ = tensor.view(-1, N*N).min(axis=1)
+    minTensor = torch.ones_like(tensor)
+    for i in range(minVals.shape[0]):
+       minTensor[i] = minTensor[i]*minVals[i]
+    return minTensor
 
 
-"""
-Saving examples of generated and ground truth images for easy viewing
-"""
+def findMax(tensor):
+    N = tensor.shape[-1]
+    maxVals, _ = tensor.view(-1, N*N).max(axis=1)
+    maxTensor = torch.ones_like(tensor)
+    for i in range(maxVals.shape[0]):
+       maxTensor[i] = maxTensor[i]*maxVals[i]
+    return maxTensor
 
 
-def saveSomeExamples(generator, validationLoader, epoch, folder):
-    x, y = next(iter(validationLoader))
-    x, y = x.to(config.DEVICE), y.to(config.DEVICE)
-    generator.eval()
-    with torch.no_grad():
-        yFake = generator(x)
-        yFake = yFake * 0.5 + 0.5  # remove normalization#
-        save_image(yFake, folder + f"/y_gen_{epoch}.png")
-        save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
-        if epoch == 1:
-            save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}.png")
-    generator.train()
+def normaliseTensor(tensor):
+    return (tensor-findMin(tensor))/findMax(tensor)
+
