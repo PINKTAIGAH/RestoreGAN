e58a0599807f482f2ebc14a6825e8abd8472d575
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-07-26 14:26:04 +0100

    Modified main section of traininig script to include new dataclass and new loss functions

--- src/train.py
+++ src/train.py
@@ -1,128 +1,114 @@
 import torch
+from utils import save_checkpoint, load_checkpoint, save_some_examples
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+import config
+from dataset import JitteredDataset  
+from generator import Generator
+from discriminator import Discriminator
 from torch.utils.data import DataLoader
-from torch.utils.tensorboard.writer import SummaryWriter
-from model import Generator, Critic, initialiseWeights
-from utils import gradientPenalty
+from tqdm import tqdm
+from torchvision.utils import save_image
 
-"""
-Defining hyperparameters
-"""
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 1e-4 
-BATCH_SIZE = 64
-IMAGE_SIZE = 64
-CHANNELS_IMAGE = 1      # 1 for mnist and 3 for RGB
-Z_DIMENTION = 100
-N_EPOCH = 100
-FEATURES_CRITIC = 16
-FEATURES_GENERATOR = 16
-CRITIC_ITERATIONS = 5
-LAMBDA_GP = 10
-
-transforms = transforms.Compose([
-    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        [0.5 for _ in range(CHANNELS_IMAGE)],   # generalise for multi channel
-        [0.5 for _ in range(CHANNELS_IMAGE)],
-    ),
-])
-
-dataset = datasets.MNIST(root="../dataset/", train=True, transform= transforms,
-                         download=True)
-loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-generator = Generator(Z_DIMENTION, CHANNELS_IMAGE, FEATURES_GENERATOR).to(device)
-critic = Critic(CHANNELS_IMAGE, FEATURES_CRITIC).to(device)
-
-initialiseWeights(critic)
-initialiseWeights(generator)
-
-optimiserGenerator = optim.Adam(generator.parameters(), lr=LEARNING_RATE,
-                                betas=(0.0, 0.9))
-optimiserCritic = optim.Adam(critic.parameters(), lr=LEARNING_RATE,
-                             betas=(0.0, 0.9))
-
-fixedNoise = torch.randn(32, Z_DIMENTION, 1, 1).to(device)
-
-writerReal = SummaryWriter(f"runs_mnist/real")
-writerFake = SummaryWriter(f"runs_mnist/fake")
-step = 0
-
-### Setting the models to training mode
-generator.train()
-critic.train()
+torch.backends.cudnn.benchmark = True
 
 
-"""
-Training loop
-"""
+def train_fn(
+    disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_scaler,
+):
+    loop = tqdm(loader, leave=True)
+    step = 0
 
-for epoch in range(N_EPOCH):
-    for batchIdx, (realImage, _) in enumerate(loader):
-        realImage = realImage.to(device)
-        currBatchSize = realImage.shape[0]
+    for idx, (x, y) in enumerate(loop):
+        x = x.to(config.DEVICE)
+        y = y.to(config.DEVICE)
 
-        for _ in range(CRITIC_ITERATIONS):
-        
-            ### Training five times: Critic  {max mean(D(x) - mean D(G(z)))}
-            noise = torch.randn((currBatchSize, Z_DIMENTION, 1, 1)).to(device)
-            fakeImage = generator(noise)
+        # Train Discriminator
+        with torch.cuda.amp.autocast():
+            y_fake = gen(x)
+            D_real = disc(x, y)
+            D_real_loss = bce(D_real, torch.ones_like(D_real))
+            D_fake = disc(x, y_fake.detach())
+            D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
+            D_loss = (D_real_loss + D_fake_loss) / 2
 
-            criticReal = critic(realImage).reshape(-1)
-            criticFake = critic(fakeImage).reshape(-1)
-            gp = gradientPenalty(critic, realImage,
-                                 fakeImage, device=device)
+        disc.zero_grad()
+        d_scaler.scale(D_loss).backward()
+        d_scaler.step(opt_disc)
+        d_scaler.update()
 
-            # We write the loss function ourselves
-            lossCritic = (
-                -(torch.mean(criticReal) - torch.mean(criticFake)) + LAMBDA_GP*gp
-            )           
-            
-            critic.zero_grad()
-            lossCritic.backward(retain_graph=True)
-            optimiserCritic.step()
+        # Train generator
+        with torch.cuda.amp.autocast():
+            D_fake = disc(x, y_fake)
+            G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
+            L1 = l1_loss(y_fake, y) * config.L1_LAMBDA
+            G_loss = G_fake_loss + L1
 
-        ### TRAINing GENERATOR      {min D(G(Z))}
-        output = critic(fakeImage).reshape(-1)
-        lossGenerator = -torch.mean(output)
+        opt_gen.zero_grad()
+        g_scaler.scale(G_loss).backward()
+        g_scaler.step(opt_gen)
+        g_scaler.update()
 
-        generator.zero_grad()
-        lossGenerator.backward()
-        optimiserGenerator.step()
-
-        ### Visualise on tensor board
-        if batchIdx % 100 == 0:
-            generator.eval()
-            critic.eval()
-
-            print(
-                f"Epoch [{epoch}/{N_EPOCH}] Batch {batchIdx}/{len(loader)} \
-                  Loss D: {lossCritic:.4f}, loss G: {lossGenerator:.4f}"
+        if idx % 10 == 0:
+            loop.set_postfix(
+                D_real=torch.sigmoid(D_real).mean().item(),
+                D_fake=torch.sigmoid(D_fake).mean().item(),
             )
 
-            with torch.no_grad():
-                fake = generator(fixedNoise)
-            
-                ### Take out (up to) 32 examples
-                imageGridReal = torchvision.utils.make_grid(
-                    realImage[:32], normalize=True
-                )
-                imageGridFake = torchvision.utils.make_grid(
-                    fake[:32], normalize=True
-                )
+#        with torch.no_grad():
+#            fakeSample = generator(x) 
+#            imageGridReal = torchvision.utils.make_grid(y[:32], normalize=True)
+#            imageGridFake = torchvision.utils.make_grid(fakeSample[:32], normalize=True)
+#
+#            config.WRITER_REAL.add_image("real", imageGridReal, global_step=step)
+#            config.WRITER_FAKE.add_image("fake", imageGridFake, global_step=step)
+#
+#            step +=1
 
-                writerReal.add_image("real", imageGridReal, global_step=step)
-                writerFake.add_image("fake", imageGridFake, global_step=step)
-            
-            step += 1
-            generator.train()
-            critic.train()
+def main():
+    disc = Discriminator(config.CHANNELS_IMG, featuresD=16).to(config.DEVICE)
+    gen = Generator(inChannel=config.CHANNELS_IMG,
+                    outChannel=config.CHANNELS_OUT,
+                    imageSize=config.IMAGE_SIZE).to(config.DEVICE)
+
+    opt_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999),)
+    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
+    LOSS_CONTENT = nn.L1Loss()
+    LOSS_JITTER = nn.MSELoss()
+
+
+    if config.LOAD_MODEL:
+        load_checkpoint(
+            config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,
+        )
+        load_checkpoint(
+            config.CHECKPOINT_DISC, disc, opt_disc, config.LEARNING_RATE,
+        )
+
+    train_dataset = JitteredDataset(config.IMAGE_SIZE, config.IMAGE_JITTER, length=1024)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS,
+    )
+    g_scaler = torch.cuda.amp.GradScaler()
+    d_scaler = torch.cuda.amp.GradScaler()
+    val_dataset = JitteredDataset(config.IMAGE_SIZE, config.IMAGE_JITTER, length=256) 
+    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+
+    for epoch in range(config.NUM_EPOCHS):
+        train_fn(
+            disc, gen, train_loader, opt_disc, opt_gen, LOSS_CONTENT, LOSS_JITTER,
+            g_scaler, d_scaler,
+        )
+
+        if config.SAVE_MODEL and epoch % 5 == 0:
+            save_checkpoint(gen, opt_gen, filename=config.CHECKPOINT_GEN)
+            save_checkpoint(disc, opt_disc, filename=config.CHECKPOINT_DISC)
+
+        save_some_examples(gen, val_loader, epoch, folder="evaluation")
                 
-writerFake.close()
-writerReal.close()
+
+if __name__ == "__main__":
+    main()
