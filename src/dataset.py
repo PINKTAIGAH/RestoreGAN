99c3f1e94331d385f09bb79e298d4c3382163ad3
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-03 15:19:36 +0100

    modified method images are generated in dataset using the new image generator class

--- src/dataset.py
+++ src/dataset.py
@@ -1,82 +1,50 @@
-import sys
 import torch
 import utils
 import config
 from torch.utils.data import Dataset, DataLoader
 from ImageGenerator import ImageGenerator
-from JitterFilter import JitterFilter
 from torchvision.utils import save_image
-import torchvision
-from torch.utils.tensorboard.writer import SummaryWriter
 
 class JitteredDataset(Dataset):
-    def __init__(self, N, maxJitter, psfSigma=3, length=128, concatImages=False):
-        self.N = N
+    def __init__(self, imageSize, length, maxJitter,):
+        self.N = imageSize 
         self.length = length
         self.maxJitter = maxJitter
-        self.Generator = ImageGenerator(self.N)
-        self.Filter = JitterFilter()
-        self.concatImages = concatImages
-        self.psfSigma = psfSigma
+        self.filter = ImageGenerator(config.PSF, config.MAX_JITTER, config.IMAGE_SIZE,)
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-        groundTruthNumpy = self.Generator.genericNoise(sigma=self.psfSigma,
-                                                       kernalSize=65)
-        jitteredTruthNumpy = self.Filter.rowJitter(groundTruthNumpy, self.N,
-                                                   self.maxJitter,)
 
-        # Image.fromarray(groundTruthNumpy*255).convert("RGB").save("x_np.png")
-        # Image.fromarray(jitteredTruthNumpy*255).convert("RGB").save("y_np.png")
+        groundTruth, whiteNoise = self.filter.generateGroundTruth()
+        shifts = self.filter.generateShifts()
+        shiftsVertical = self.filter.generateShifts()
+        shiftedImage = self.filter.shiftImage(groundTruth, shifts)
+        shiftedImageVertical = self.filter.verticalShiftImage(shiftedImage,
+                                                      shiftsVertical)
 
-        groundTruthTorch = torch.tensor(groundTruthNumpy, dtype=torch.float32) 
-        jitteredTruthTorch = torch.tensor(jitteredTruthNumpy, dtype=torch.float32) 
-        jitterVector = torch.unsqueeze(torch.tensor(-self.Filter.jitterVector), 0)
+        groundTruth = torch.unsqueeze(groundTruth, 0)
+        shiftedImage = torch.unsqueeze(shiftedImage, 0)
+        shiftedImageVertical = torch.unsqueeze(shiftedImageVertical, 0)
 
-        groundTruthTorch = torch.unsqueeze(groundTruthTorch, 0)
-        jitteredTruthTorch = torch.unsqueeze(jitteredTruthTorch, 0)
+        shiftedImage = utils.normaliseTensor(shiftedImage)
+        shiftedImageVertical = utils.normaliseTensor(shiftedImageVertical)
+        groundTruth = utils.normaliseTensor(groundTruth)
 
-        if self.concatImages:
-            return utils.tensorConcatinate(jitteredTruthTorch, groundTruthTorch)
+        shiftedImage = config.transforms(shiftedImage)
+        groundTruth = config.transforms(groundTruth)
+        shiftedImageVertical = config.transforms(shiftedImageVertical)
 
-        jitteredTruthTorch = utils.rescaleTensor(jitteredTruthTorch)
-        groundTruthTorch = utils.rescaleTensor(groundTruthTorch)
-
-        jitteredTruthTorch = config.transforms(jitteredTruthTorch)
-        groundTruthTorch = config.transforms(groundTruthTorch)
-
-        return jitteredTruthTorch, groundTruthTorch, jitterVector
+        return shiftedImageVertical, groundTruth
 
 if __name__ == "__main__":
-    filter = JitterFilter()
-    writerJittered = SummaryWriter("test/jittered")
-    writerUnjittered = SummaryWriter("test/unjittered")
-    writerDejittered = SummaryWriter("test/dejittered")
 
     N = 256 
-    dataset = JitteredDataset(N, 5)
+    dataset = JitteredDataset(N, 20, 2)
     loader = DataLoader(dataset, batch_size=5)
-    # sys.exit()
-    for x, y, vector in loader:
-        
-        xDejittered = filter.rowDejitterBatch(x, vector) 
 
-        imgGridJittered = torchvision.utils.make_grid(x, normalize=True)
-        imgGridUnjittered = torchvision.utils.make_grid(y, normalize=True)
-        imgGridDejittered = torchvision.utils.make_grid(xDejittered, normalize=True)
-        
-        writerJittered.add_image("Jittered", imgGridJittered)
-        writerUnjittered.add_image("Unjittered", imgGridUnjittered)
-        writerDejittered.add_image("Dejittered", imgGridDejittered)
+    for x, y in loader:
 
-        x = x*0.5+0.5
-        y = y*0.5+0.5
-        xDejittered = xDejittered*0.5+0.5
-
-        save_image(x, "images/Jittered.tiff")
-        save_image(y, "images/Unjittered.tiff")
-        save_image(xDejittered, "images/Dejittered.tiff")
-
-        sys.exit()
+        save_image(x, "images/Jittered.png")
+        save_image(y, "images/Unjittered.png")
