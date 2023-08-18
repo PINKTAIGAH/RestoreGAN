6efcb9cd2146ab669497b4e7737360d9f2196e55
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-18 10:56:19 +0100

    Added coment to discriminator

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,12 +1,26 @@
 import torch
 import torch.nn as nn
-from torch.nn.modules import padding
-
-"""
-Model for Discriminator
-"""
 
 class Discriminator(nn.Module):
+    """
+    A torch.nn.Module instance containing a Deep Convolutional GAN network designed
+    to discriminate images of size 128*128. Architecture of this network is based
+    on the folliwing paper (https://doi.org/10.48550/arXiv.1511.06434)
+
+    Atribute
+    --------
+    critic: torch.nn.Sequential instance
+        Object that will return the output of the DCGAN discriminator
+
+    Parameters
+    ----------
+    channelImages: int
+        Number of colour channels in discriminator input
+
+    featuresD: int
+        Coefficient that will scale the number of channels created at each 
+        convolutional block of the discriminator
+    """
 
     def __init__(self, channelImages, featuresD):
         super(Discriminator, self).__init__()
@@ -30,8 +44,39 @@ class Discriminator(nn.Module):
     
     ### Convolutional block that contains BatchNorm2d after a convolution
     def _block(self, inChannels, outChannels, kernalSize, stride, padding):
-        ### This will be internal blocks of layers of the model
-        ### In and out channels are the channel input and output size
+        """
+        Convolutional block that contains a batch normalisation after the 
+        convolution of the input tensor.
+
+        Parameters
+        ----------
+        inChannels: int
+            Number of image channels in input image
+
+        outChannels: int
+            Number of image channels that output image will have
+
+        kernalSize: int
+            Hight and width of convolutional kernal
+
+        stride: int
+            Stride of convolution
+
+        padding: int
+            Hight and width of padding to be added to image prior to convolution
+
+        Returns
+        -------
+        output: torch.nn.Sqeuential instance
+            Instance of Sequential class containing all steps required for 
+            convolutional block
+
+        Notes
+        -----
+        These convolutional blocks contain a batch normalisation. This is inline
+        with the internal layers of the DCGAN model, as oposed to the initial and 
+        final convilutional blocks (which do not contain a batch normalisation)
+        """
         return nn.Sequential(
             nn.Conv2d(inChannels, outChannels, kernalSize, 
                       stride, padding, bias=False),
@@ -40,14 +85,30 @@ class Discriminator(nn.Module):
         )
 
     def forward(self, x):
+        """
+        Returns output of DCGAN discriminator model when called
+
+        Parameters
+        ----------
+        x: torch.FloatTensor
+            Input tensor to be passed through discriminator
+
+        Returns
+        -------
+        output: torch.FloatTensor
+            Tensor containing discriminator score of the inputted image
+        """
         return self.critic(x)
 
-"""
-Initialise weights for discriminator
-"""
-
 def initialiseWeights(model):
-    ### Setting init weights when doing one of the following operations
+    """
+    Initialise weights of discriminator  
+
+    Parameters
+    ----------
+    model: torch.nn.Module instance
+        Model object whose weights will be initialised 
+    """
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
