7b406e851b0cf667e6a2f291f087b1f1dfacec39
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-18 10:03:33 +0100

    Added comments to config file

--- src/config.py
+++ src/config.py
@@ -5,10 +5,42 @@ from torch.utils.tensorboard.writer import SummaryWriter
 from torchvision.transforms import transforms as transform
 
 def _normalise(x):
+    """
+    Normalise a ndArray
+
+    Parameters
+    ----------
+    x: ndArray
+        Input array containing dataset to be normalised
+
+    Returns
+    -------
+    output: ndArray
+        Normalised array
+    """
     if np.sum(x) == 0:
         raise Exception("Divided by zero. Attempted to normalise a zero tensor")
 
     return x/np.sum(x**2)
+
+def _generatePointFunction():
+    """
+    Generate a image array of a point function with size of defined in config 
+    file (excluding padding)
+    
+    Returns
+    -------
+    pointFunction: ndArray
+        2D image array containing a point function
+    """
+
+    pointFunction = np.zeros((NOISE_SIZE, NOISE_SIZE))
+    pointFunction[NOISE_SIZE//2, NOISE_SIZE//2] = 1
+    return pointFunction
+
+"""
+Hyper parameters
+"""
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TRAIN_DIR = "/home/giorgio/Desktop/p06_images/train/"
@@ -20,11 +52,11 @@ LEARNING_RATE = 1e-4
 SCHEDULAR_DECAY = 0.5
 SCHEDULAR_PATIENCE = 20
 NUM_WORKERS = 2
-MAX_JITTER = 4
-PADDING_WIDTH = 30
+MAX_JITTER = 3
+PADDING_WIDTH = 15
 IMAGE_SIZE = 128 
 NOISE_SIZE = IMAGE_SIZE - PADDING_WIDTH*2
-SIGMA = 20
+SIGMA = 10
 CHANNELS_IMG = 1 
 CHANNELS_OUT = 2
 CORRELATION_LENGTH = 10
@@ -32,19 +64,23 @@ NUM_EPOCHS =  1000
 LAMBDA_CONTENT = 1.0
 LAMBDA_JITTER = 1.0
 LAMBDA_GP = 10
-SCALING_FACTOR = 6
 LOAD_MODEL = False 
 SAVE_MODEL = True
 CHECKPOINT_DISC = "../models/disc.pth.tar"
 CHECKPOINT_GEN = "../models/gen.pth.tar"
+
 WRITER_REAL = SummaryWriter("../runs/real")
 WRITER_FAKE = SummaryWriter("../runs/fake")
 # WRITER_REAL = SummaryWriter("/home/brunicam/myscratch/p3_scratch/runs/real")
 # WRITER_FAKE = SummaryWriter("/home/brunicam/myscratch/p3_scratch/runs/fake")
 
-kernal = np.zeros((NOISE_SIZE, NOISE_SIZE))
-kernal[NOISE_SIZE//2, NOISE_SIZE//2] = 1
-PSF = torch.from_numpy(_normalise(gaussian(kernal, SIGMA))).type(torch.float32)
+# Generate a point spread finction by applying a gaussian blur to a point function
+pointFunction = _generatePointFunction()
+PSF = torch.from_numpy(_normalise(gaussian(pointFunction, SIGMA))).type(torch.float32)
+
+"""
+Tensor transforms
+"""
 
 transforms = transform.Compose([
     transform.Normalize(
@@ -53,7 +89,7 @@ transforms = transform.Compose([
     ),
 ])
 
-transformsCell = transform.Compose([
+transformsFile = transform.Compose([
     transform.ToTensor(),
     transform.RandomCrop(IMAGE_SIZE),
     transform.Grayscale(),
