e01c444f6bef0c66c28d28e9ddfe00fb1a2ec3cf
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-08-20 22:06:43 +0100

    Added content and jitter loss

--- src/train.py
+++ src/train.py
@@ -121,17 +121,14 @@ def _trainFunction(
             output = disc(img_fake).reshape(-1)
             loss_adverserial_gen = -torch.mean(output)
 
-            ### Ignore ###
-            # loss_content = content_loss(img_truth, img_fake)
-            # loss_jitter = jitter_loss(unshift_map_truth, unshift_map_fake)
+            loss_content = content_loss(img_truth, img_fake)
+            loss_jitter = jitter_loss(unshift_map_truth, unshift_map_fake)
 
             # Compute overall loss function of discriminator
-            # loss_gen = (
-                # loss_adverserial_gen + loss_content*config.LAMBDA_CONTENT + 
-                # loss_jitter*config.LAMBDA_JITTER
-            # )
-            ### Ignore ###
-            loss_gen = loss_adverserial_gen
+            loss_gen = (
+                loss_adverserial_gen + loss_content*config.LAMBDA_CONTENT + 
+                loss_jitter*config.LAMBDA_JITTER
+            )
 
         # Zero gradients of discriminator to avoid old gradients affecting backwards
         # pass
